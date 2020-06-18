29143e59727c311a3fd4518a5f3acb062c2a68d8
Dssaenz <darwinsaenz105@gmail.com>
2020-06-17 23:30:52 -0500

    add redux and principal state

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import {
   Search,
   Categories,
@@ -8,20 +8,15 @@ import {
 } from '../components';
 
 import '../assets/styles/App.scss';
-import useInitialState from '../hooks/useInitialState';
 
-const API = 'http://localhost:3000/initialState';
-
-function Home() {
-  const initialState = useInitialState(API);
-  console.log(initialState, 'el stado');
+function Home({ mylist, trends, originals }) {
   return (
     <>
       <Search />
-      {initialState.mylist?.length > 0 && (
+      {mylist.length > 0 && (
         <Categories title='Mi lista'>
           <Carousel>
-            {initialState.mylist?.map((item) => (
+            {mylist?.map((item) => (
               <ItemCarousel
                 key={item.id}
                 title={item.title}
@@ -36,7 +31,7 @@ function Home() {
       )}
       <Categories title='Tendencias'>
         <Carousel>
-          {initialState.trends?.map((item) => (
+          {trends.map((item) => (
             <ItemCarousel
               key={item.id}
               title={item.title}
@@ -50,7 +45,7 @@ function Home() {
       </Categories>
       <Categories title='Originales'>
         <Carousel>
-          {initialState.originals?.map((item) => (
+          {originals.map((item) => (
             <ItemCarousel
               key={item.id}
               title={item.title}
@@ -66,4 +61,12 @@ function Home() {
   );
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    mylist: state.mylist,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './routes/App';
+import store from '../store';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('app'),
+);
 
